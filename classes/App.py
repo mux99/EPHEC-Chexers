6dc3e8363cf7d45ddf7c6ec15e8057824a149330
maxime <mux99@live.be>
2022-10-20 15:38:45 +0200

    Added Window + background board

--- classes/App.py
+++ classes/App.py
@@ -2,3 +2,4 @@ class App():
 	"""docstring for App"""
 	def __init__(self):
 		self.pieces = []
+		self.player = 0
--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import pyglet
+from pyglet import clock, font, image, window
+from pyglet.window import key, Window
+
+win = window.Window(fullscreen=True,caption="Checkers")
+back = pyglet.sprite.Sprite(pyglet.resource.image("img/board.png"),0,0)
+back.scale = win.get_size()[1] /back.height
+print(back.height,win.get_size())
+
+@win.event
+def on_draw():
+	win.clear()
+	back.draw()
+
+def update(delta_time):
+	win.clear()
+	back.draw()
+
 if __name__ == '__main__':
-	pass
+	pyglet.clock.schedule_interval(update,1/60)
+	pyglet.app.run()
