efa1c21bdae20a73908c5e1d8e8c89fc057c0407
maxime <mux99@live.be>
2022-10-21 12:18:21 +0200

    added corect caling for pawns (not in class)

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from pyglet.window import key, Window
 
 win = window.Window(fullscreen=True,caption="Checkers")
 back = pyglet.sprite.Sprite(pyglet.resource.image("img/board.png"),0,0)
-back.scale = win.get_size()[1] /back.height
-print(back.height,win.get_size())
+
+white = pyglet.sprite.Sprite(pyglet.resource.image("img/white.png"),0,0)
+
+scale = win.get_size()[1] / back.height
+print(scale)
+back.scale = scale
+white.scale = scale - 0.25
 
 @win.event
 def on_draw():
 	win.clear()
 	back.draw()
 
+	white.draw()
+
 def update(delta_time):
-	win.clear()
-	back.draw()
+	pass
 
 if __name__ == '__main__':
 	pyglet.clock.schedule_interval(update,1/60)
