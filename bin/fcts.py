228baf9db273ccbc618937429a76a49123fa7775
MIKObscura <contact.mikobscura@gmail.com>
2022-10-31 15:14:52 +0100

    screen_to_board

--- bin/fcts.py
+++ bin/fcts.py
@@ -2,14 +2,22 @@ from math import sqrt
 """
 translate the coord on screen to x,y,z hexagonal axis
 """
-def screen_to_board(x, y):
-	return (0,0,0)
+def screen_to_board(x, y, window):
+	tile_height = window.height / 6.25
+	board_x = (y - (tile_height * 0.5)) / (tile_height * 0.75)
+	board_y = -((x + (board_x % 2 * tile_height * sqrt(0.1875) - (tile_height * 4 * sqrt(0.1875)))) / ((tile_height / 8) * (sqrt(3) / 2))) + ((y - (tile_height * 0.5)) / (tile_height * 0.375))
+	board_z = -(board_x + board_y)
+	return (int(board_x), int(board_y), int(board_z))
 
 
 """
 translate the coords of hex tiles to their screen coords
 """
 def board_to_screen(x, y, z):
+	"""
+	screen_x = ((z-y) * (tile_height / 4) * sqrt(3)) - (x%2 * tile_height * sqrt(0.1875)) + (tile_height * 4 * sqrt(0.1875)
+	screen_y = (x * tile_height * 0.75 + (tile_height * 0.5))
+	"""
 	return (int(z-y),int(x))
 
 """
